06a5cb864a9ea2f37da329273fd62a28ec547191
LuxGiammi <LuxGiammi@users.noreply.github.com>
2017-04-24 15:14:15 +0200

    Finishing functions: does NOT work!

--- SudokuSolver/main.c
+++ SudokuSolver/main.c
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
@@ -11,18 +12,18 @@ bool checkRow(uchar sudoku[][SUDOKU_SIZE], size_t r, uchar num) {
 	for (size_t i = 0; i < SUDOKU_SIZE; i++)
 	{
 		if (sudoku[r][i] == num)
-			return true;
+			return false;
 	}
-	return false;
+	return true;
 }
 
 bool checkColumn(uchar sudoku[][SUDOKU_SIZE], size_t c, uchar num) {
 	for (size_t i = 0; i < SUDOKU_SIZE; i++)
 	{
 		if (sudoku[i][c] == num)
-			return true;
+			return false;
 	}
-	return false;
+	return true;
 }
 
 bool checkSubmatrix(uchar sudoku[][SUDOKU_SIZE], size_t r, size_t c, uchar num) {
@@ -35,30 +36,73 @@ bool checkSubmatrix(uchar sudoku[][SUDOKU_SIZE], size_t r, size_t c, uchar num) 
 		{
 			//Getting cell in sudoku matrix: [sr * 3][sc*3] is the top-left cell of the submatrix.
 			if (sudoku[sr * 3 + isr][sc * 3 + isc] == num) {
-				return true;
+				return false;
 			}
 		}
 	}
 
-	return false;
+	return true;
 }
 
 void sudokuSolver(uchar sudoku[][SUDOKU_SIZE], size_t r, size_t c, bool foundSolution) {
 	//Read the matrix by rows: if I have r >= SUDOKU_SIZE, I increment the column and reset the row number
-	if (r >= SUDOKU_SIZE) {
-		r = 0;
-		c++;
+	if (c >= SUDOKU_SIZE) {
+		c = 0;
+		r++;
 	}
 
 	//If the c >= SUDOKU_SIZE, I've solved the sudoku
-	if (c >= SUDOKU_SIZE)
+	if (r >= SUDOKU_SIZE)
 	{
 		foundSolution = true;
 	}
 
 	//I'm considering row = r, and column = c:
 	//Try to put here a number
+	if (sudoku[r][c] == 0) {
+		for (uchar i = 1; i <= 9; i++)
+		{
+			if (checkRow(sudoku, r, i) && checkColumn(sudoku, c, i) && checkSubmatrix(sudoku, r, c, i)) {
+				//i can go in [r][c]
+				sudoku[r][c] = i;
+				//Recursive call for sudoku
+				sudokuSolver(sudoku, r, c+1, foundSolution);
+				if (foundSolution)
+					return;			//No need to continue: return.
+			}
+		}
+		return;
+	}
+	else {
+		sudokuSolver(sudoku, r, c+1, foundSolution);
+	}
+}
 
+void LoadSudokuFromFile(char *filename, uchar sudoku[][SUDOKU_SIZE]) {
+	FILE *f = fopen(filename, "rt");
+	for (size_t r = 0; r < SUDOKU_SIZE; r++)
+	{
+		for (size_t c = 0; c < SUDOKU_SIZE; c++)
+		{
+			int n;
+			fscanf(f, "%d ", &n);
+			sudoku[r][c] = (uchar)n;
+		}
+	}
+	fclose(f);
+}
+
+void PrintSudokuToFile(char *filename, uchar sudoku[][SUDOKU_SIZE]) {
+	FILE *f = fopen(filename, "wt");
+	for (size_t r = 0; r < SUDOKU_SIZE; r++)
+	{
+		for (size_t c = 0; c < SUDOKU_SIZE; c++)
+		{
+			fprintf(f, "%d ", sudoku[r][c]);
+		}
+		fputc('\n', f);
+	}
+	fclose(f);
 }
 
 int main() {
@@ -89,7 +133,7 @@ int main() {
 		1. Each row contains all numbers from 1 to 9 exactly once.
 		2. Each column contains all numbers from 1 to 9 exactly once.
 		3. Each submatrix contains all numbers from 1 to 9 exactly once.
-	
+
 	Some hints are given at start (some cells are already filled).
 	The problem is: fill the remaining cells following the rules given before.
 	*/
@@ -98,4 +142,13 @@ int main() {
 
 	//Fill sudoku in any way.
 
+	LoadSudokuFromFile("example.sudoku", sudoku);
+
+	//Call sudoku:
+	sudokuSolver(sudoku, 0, 0, false);
+
+	//Print solution to a file:
+	PrintSudokuToFile("solution.sudoku", sudoku);
+
+	return 0;
 }
